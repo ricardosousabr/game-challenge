9e5270f17691cc6ee9dbddce9b436203a3000ea1
ricardosousabr <ricardo.br.pi@gmail.com>
2026-01-14 18:36:55 -0300

    first commit

---
+++ src/main/java/com/gamechallenge/api/game_challenge/GamifiedChallengesApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamechallenge.api.game_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamifiedChallengesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamifiedChallengesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamechallenge/api/game_challenge/GamifiedChallengesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamechallenge.api.game_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamifiedChallengesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
