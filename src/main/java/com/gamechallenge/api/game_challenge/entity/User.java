dcfc7b12c86a7a362ea6e149f77e0ca2c77a71ec
ricardosousabr <ricardo.br.pi@gmail.com>
2026-01-16 20:33:40 -0300

    feat: update end entity user

--- src/main/java/com/gamechallenge/api/game_challenge/entity/User.java
+++ src/main/java/com/gamechallenge/api/game_challenge/entity/User.java
@@ -3,7 +3,7 @@ package com.gamechallenge.api.game_challenge.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table
+@Table(name = "user")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
