f7e6a19471fdafc817e59b81bb22dba3c57dfa92
ricardosousabr <ricardo.br.pi@gmail.com>
2026-01-21 21:10:07 -0300

    feat: add getter and setter for entity

--- src/main/java/com/gamechallenge/api/game_challenge/entity/Challenge.java
+++ src/main/java/com/gamechallenge/api/game_challenge/entity/Challenge.java
@@ -1,7 +1,11 @@
 package com.gamechallenge.api.game_challenge.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "challenge")
 public class Challenge {
--- src/main/java/com/gamechallenge/api/game_challenge/entity/UserChallenge.java
+++ src/main/java/com/gamechallenge/api/game_challenge/entity/UserChallenge.java
@@ -1,9 +1,13 @@
 package com.gamechallenge.api.game_challenge.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "user_challenge")
 public class UserChallenge {
