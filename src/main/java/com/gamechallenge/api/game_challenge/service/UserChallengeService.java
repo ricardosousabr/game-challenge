1e852d5b60103612655b5805124a0cb6349d1964
ricardosousabr <ricardo.br.pi@gmail.com>
2026-01-28 19:41:03 -0300

    feat: add exception in service

--- src/main/java/com/gamechallenge/api/game_challenge/service/UserChallengeService.java
+++ src/main/java/com/gamechallenge/api/game_challenge/service/UserChallengeService.java
@@ -1,10 +1,15 @@
 package com.gamechallenge.api.game_challenge.service;
 
+import com.gamechallenge.api.game_challenge.dto.ChallengeCompletionResponse;
+import com.gamechallenge.api.game_challenge.entity.ErrorCode;
 import com.gamechallenge.api.game_challenge.entity.User;
 import com.gamechallenge.api.game_challenge.entity.UserChallenge;
+import com.gamechallenge.api.game_challenge.exception.BusinessException;
 import com.gamechallenge.api.game_challenge.repository.UserChallengeRepository;
 import com.gamechallenge.api.game_challenge.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,9 +24,10 @@ public class UserChallengeService {
 	@Autowired
 	UserChallengeRepository userChallengeRepository;
 	
-	public void completeChallenge(Long userId, Long userChallengeId) {
-		User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
-		UserChallenge userChallenge = userChallengeRepository.findById(userChallengeId).orElseThrow(() -> new RuntimeException("Error"));
+	@Transactional
+	public ChallengeCompletionResponse completeChallenge(Long userId, Long userChallengeId) {
+		User user = userRepository.findById(userId).orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+		UserChallenge userChallenge = userChallengeRepository.findById(userChallengeId).orElseThrow(() -> new BusinessException(ErrorCode.INVALID_REQUEST));
 		
 		levelService.validateCompletion(user, userChallenge);
 		levelService.markAsCompleted(userChallenge);
@@ -31,5 +37,8 @@ public class UserChallengeService {
 		
 		userRepository.save(user);
 		userChallengeRepository.save(userChallenge);
+		
+		return new ChallengeCompletionResponse(user.getId(), userChallenge.getChallenge().getId(), xp, user.getXp(), user.getLevel(), userChallenge.getStatus(), userChallenge.getCompletedAt());
+		
 	}
 }
